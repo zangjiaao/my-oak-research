8fc09e2617ca589418a20a93f3f8e661f090ff7c
zangjiaao <zangjiaao@yahoo.com>
2026-03-31 12:39:33 +0800

    fix(web): prevent main content overlap on small viewports

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -59,7 +59,7 @@ export default async function RootLayout({
               <SidebarInset>
                 <Toaster richColors />
                 <Headbar />
-                <main className="flex flex-1 flex-col gap-4 p-4">
+                <main className="flex min-w-0 flex-1 flex-col gap-4 p-4">
                   {children}
                 </main>
               </SidebarInset>
